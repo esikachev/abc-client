ea85787cbd9c5ac9d4a3f14b232d615a11923ba4
Evgeny Sikachev <esikachov@gmail.com>
2017-07-20 19:00:42 +0000

    Fixed typo

--- abc_client/client/client.py
+++ abc_client/client/client.py
@@ -6,14 +6,16 @@ class Client(object):
     def __init__(self):
         self.host = 'http://127.0.0.1:5000'
 
-    def _post(self, *args, **kwargs):
+    @staticmethod
+    def _post(*args, **kwargs):
         return post(*args, **kwargs)
 
-    def _get(self, *args):
+    @staticmethod
+    def _get(*args):
         return get(*args)
 
     def post(self, prefix, *args, **kwargs):
-        request = self._post(self.get_url(prefix), *args, **kwargs)
+        request = Client._post(self.get_url(prefix), *args, **kwargs)
         return request.json()
 
     def get_url(self, prefix):
