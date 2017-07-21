9041d70beecb4d6350ef17241bd16b7d2807e0a6
Evgney Sikachev <esikachov@gmail.com>
2017-07-21 16:52:39 +0400

    Update prepare-server.sh

--- travis-ci-scripts/prepare-server.sh
+++ travis-ci-scripts/prepare-server.sh
@@ -1,17 +1,14 @@
-#!/bin/bash set -xe
+#!/bin/bash -xe
 
-if [ ${TEST_FUNCTIONAL} = "true" ]
-then
-  git clone https://github.com/esikachev/abc-server
-  cd abc-server
-  tox -e venv -- abc-server -e ${GITHUB_USERNAME} -p ${GITHUB_PASSWORD} &> /tmp/server.log &
-  while true
-  do
-    if [ -f /tmp/server.log ]
-    then
-      if [ "$(grep -c "Running on" /tmp/server.log)" == 1 ]; then break; fi
-    fi
-    sleep 2
-  done
-  cd -
- fi
+git clone https://github.com/esikachev/abc-server
+cd abc-server
+tox -e venv -- abc-server -e ${GITHUB_USERNAME} -p ${GITHUB_PASSWORD} &> /tmp/server.log &
+while true
+do
+  if [ -f /tmp/server.log ]
+  then
+    if [[ "$(grep -c "Running on" /tmp/server.log)" == 1 ]]; then break; fi
+  fi
+  sleep 2
+cd -
+fi
