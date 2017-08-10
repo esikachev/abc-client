f6975932a5aba9c41650d228a3b21778a7e8b55d
Evgney Sikachev <esikachov@gmail.com>
2017-08-10 20:40:49 +0400

    Added funtional tests for client (#13)
    * Added funtional tests for client
    
    * Update .travis.yml
    
    * Update prepare-server.sh

---
+++ travis-ci-scripts/prepare-server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+
+git clone https://github.com/esikachev/abc-server
+cd abc-server
+tox -e venv -- abc-server -e ${GITHUB_EMAIL} -p ${GITHUB_PASSWORD} &> /tmp/server.log &
+while true
+do
+  if [ -f /tmp/server.log ]
+  then
+    if [[ "$(grep -c "Running on" /tmp/server.log)" == 1 ]]; then break; fi
+  fi
+  sleep 2
+done
+cd -
