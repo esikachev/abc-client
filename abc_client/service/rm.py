b0390aba4358b13cf183c9ccb68f1edc23467f86
Evgeny Sikachev <esikachov@gmail.com>
2017-08-14 23:21:45 +0400

    Added ability to remove file from watcher
    close #18

--- abc_client/service/rm.py
+++ abc_client/service/rm.py
@@ -1,9 +1,11 @@
+import os
+
 from abc_client import settings
 from abc_client import utils
 
 
-def add(user_file):
+def rm(user_file):
+    os.remove(user_file)
     user_file = utils.check_path(user_file)
     with utils.YamlEditor(settings.CONFIG_PATH) as yaml:
-        if user_file not in yaml['watch']:
-            yaml['watch'].append(user_file)
+        yaml['watch'].remove(user_file)
